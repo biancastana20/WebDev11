4002ff1633c8ce71a5c480b26f97b7734eb7d11f
Bianca Stana <stanabianca.e94@gmail.com>
2020-09-01 11:03:54 +0300

    update

--- PhoneBook/app.js
+++ PhoneBook/app.js
@@ -1,13 +1,20 @@
-
+  
 
 var button = document.getElementById("buttonAddContact")
 button.addEventListener("click", addContact)
+var name= document.getElementById("name")
+var phone= document.getElementById("phone")
+var table = document.getElementById("myTable");
+
+var rIndex, table = document.getElementById("myTable")
+var table = document.getElementById("myTable");
+var rIndex;
 
 function addContact() {
-    
-    var table = document.getElementById("myTable");
-    var name= document.getElementById("name")
-    var phone= document.getElementById("phone")
+  var name= document.getElementById("name")
+  var table = document.getElementById("myTable");
+  var phone= document.getElementById("phone")
+
   //add row to initial table
     var row = table.insertRow();
     row.setAttribute("id", "rowId")
@@ -19,15 +26,69 @@ function addContact() {
     cell1.innerHTML = name.value;
     cell2.innerHTML = phone.value;
     cell3.innerHTML = '<a href="#delete" onclick="deleteContact(this)">Delete</a>'
-    cell4.innerHTML = '<a href="#edit" onclick="editContact(this)" >Edit</a>'
+    cell4.innerHTML = '<a href="#edit" onclick = "selectRowToInput ()" >Edit</a>'
     
     var hiddenPart=document.querySelector(".box-agendaTable")
     hiddenPart.style.visibility="visible"
     document.querySelector(".form-inline").reset();
-   
+    //  selectRowToInput ()
+    
+}
+//search index of row
+function selectRowToInput (){
+  var name= document.getElementById("name")
+  var phone= document.getElementById("phone")
+    var rIndex, table =document.getElementById("myTable")
+  for (var i =1; i< table.rows.length; i++){
+    table.rows[i].onclick = function (){
+      //get the selected row index
+      rIndex= this.rowIndex;
+      // console.log(rIndex)
+      name.value = this.cells [0].innerHTML
+      phone.value = this.cells [1].innerHTML
+      return rIndex;
+    }  ;
+  }
+  var button = document.getElementById("buttonAddContact")
+  button.addEventListener("click", editContact)
+  button.removeEventListener("click", addContact)
+  
+  function editContact(x) {
+    var name= document.getElementById("name")
+    var phone= document.getElementById("phone")
+    var table = document.getElementById("myTable");
+    
+    table.rows[rIndex].cells[0].innerHTML=name.value;
+    table.rows[rIndex].cells[1].innerHTML=phone.value;
+    // deleteInputs()
+    document.querySelector(".form-inline").reset();
+    var button = document.getElementById("buttonAddContact")
+    button.addEventListener("click", addContact)
+    button.removeEventListener("click", editContact)
+    } 
+  
 }
 
-function deleteContact (rowI){
-   var i = rowI.parentNode.parentNode.rowIndex;
-    document.getElementById("myTable").deleteRow(i);
-}
+function deleteContact (x){
+  var table = document.getElementById("myTable")
+  var i = x.parentNode.parentNode.rowIndex;
+  table.deleteRow(i)
+  // deleteInputs()
+  document.querySelector(".form-inline").reset();
+  }
+
+ 
+    // function deleteInputs() {
+    //   document.getElementById("name").value=""
+    //   document.getElementById("phone").value=""
+    // }
+    
+  // add enter option to add contact 
+  var phone= document.getElementById("phone")
+  phone.addEventListener("keyup", eventEnter)
+  function eventEnter (event){
+      if (event.keyCode === 13 ) { //key of 13 is enter in keyboard
+          // event.preventDefault (); //cancel default action of enter key
+          document.getElementById("buttonAddContact").click()
+      }
+    }
